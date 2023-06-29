cfd4dd96557b8172ae8e77101c6498e6099bf310
willy836 <wilsonkimiri836@gmail.com>
2023-06-29 19:41:42 +0300

    Hash user password

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ app.use(express.json());
 // extra packages
 
 // routes
-app.use("api/v1/auth", authRouter);
-app.use("api/v1/jobs", jobsRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
